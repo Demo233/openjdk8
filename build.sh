fa4fd38296d123a9c0868fe3d13bc293bd55178d
reverence <reverence_cz@163.com>
2020-03-14 00:22:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 
 export LANG=C
+unset JAVA_HOME
+unset CLASSPATH
 export path=/usr/local/jdk1.7.80/bin:$path
 
 bash ./configure --with-target-bits=64 --with-debug-level=slowdebug -enable-debug-symbols ZIP_DEBUGINFO_FILES=0
